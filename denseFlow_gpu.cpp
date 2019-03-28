48ab4e330ba0f05bb88d7c8885e5b73def6c166a
fengyu19 <853159963@qq.com>
2019-03-28 15:33:05 -0700

    changed the denseFlow_gpu for new dataset

--- denseFlow_gpu.cpp
+++ denseFlow_gpu.cpp
@@ -5,6 +5,13 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <sys/types.h>
+#include <vector>
+#include <dirent.h>
+#include <sys/stat.h>
+using namespace std;
 using namespace cv;
 using namespace cv::gpu;
 
@@ -33,45 +40,61 @@ static void drawOptFlowMap(const Mat& flow, Mat& cflowmap, int step,double, cons
         }
 }
 
-int main(int argc, char** argv){
-	// IO operation
-	const char* keys =
+void GetFileNames(string path, vector<string>& filenames)
+{
+	DIR *pDir;
+	struct dirent* ptr;
+	if(!(pDir = opendir(path.c_str())))
+		return;
+	while((ptr = readdir(pDir)) != 0)
+	{
+		string str = "DIR";
+		if(strcmp(ptr->d_name, ".") != 0 && strcmp(ptr->d_name, "..") != 0)
 		{
-			"{ f  | vidFile      | ex2.avi | filename of video }"
-			"{ x  | xFlowFile    | flow_x | filename of flow x component }"
-			"{ y  | yFlowFile    | flow_y | filename of flow x component }"
-			"{ i  | imgFile      | flow_i | filename of flow image}"
-			"{ b  | bound | 15 | specify the maximum of optical flow}"
-			"{ t  | type | 0 | specify the optical flow algorithm }"
-			"{ d  | device_id    | 0  | set gpu id}"
-			"{ s  | step  | 1 | specify the step for frame sampling}"
-		};
+			string v_name = ptr->d_name;
+			if(v_name.find(str) < v_name.length())
+				continue;
+			else{
+				filenames.push_back(path + ptr->d_name);
+			}
+		}
+	}
+	closedir(pDir);
+	
+}
 
-	CommandLineParser cmd(argc, argv, keys);
-	string vidFile = cmd.get<string>("vidFile");
-	string xFlowFile = cmd.get<string>("xFlowFile");
-	string yFlowFile = cmd.get<string>("yFlowFile");
-	string imgFile = cmd.get<string>("imgFile");
-	int bound = cmd.get<int>("bound");
-        int type  = cmd.get<int>("type");
-        int device_id = cmd.get<int>("device_id");
-        int step = cmd.get<int>("step");
-
-	VideoCapture capture(vidFile);
+void PorcessOneVideo(string videoFile, int device_id, int proposed_imageNum, int bound){
+	VideoCapture capture(videoFile);
 	if(!capture.isOpened()) {
 		printf("Could not initialize capturing..\n");
-		return -1;
 	}
+
+	// true skip steps
+	double fps = capture.get(CV_CAP_PROP_FPS);
+	int fps_1 = int(fps);
+	if(fps_1 == 29)
+		fps_1 = 30;
+
+	long nFrame = capture.get(CV_CAP_PROP_FRAME_COUNT);
+	int duration = nFrame/fps_1;
+	if(duration > 30)
+		duration = 30;
+
+	int step = fps_1 * duration/proposed_imageNum;
+	cout<<step<<endl;
+
+	// mkdir directory to save the optical flow output image x, y
+	string dirName = videoFile + "DIR";
+	mkdir(dirName.c_str(), S_IRWXU);
 
 	int frame_num = 0;
 	Mat image, prev_image, prev_grey, grey, frame, flow_x, flow_y;
 	GpuMat frame_0, frame_1, flow_u, flow_v;
 
 	setDevice(device_id);
-	FarnebackOpticalFlow alg_farn;
 	OpticalFlowDual_TVL1_GPU alg_tvl1;
-	BroxOpticalFlow alg_brox(0.197f, 50.0f, 0.8f, 10, 77, 10);
-
+	// we just want to capture #proposed_imageNum of pics
+	int k = 0;
 	while(true) {
 		capture >> frame;
 		if(frame.empty())
@@ -88,67 +111,85 @@ int main(int argc, char** argv){
 			frame_num++;
 
 			int step_t = step;
-			while (step_t > 1){
+			while(step_t > 1){
 				capture >> frame;
 				step_t--;
 			}
 			continue;
 		}
-
 		frame.copyTo(image);
 		cvtColor(image, grey, CV_BGR2GRAY);
 
-               //  Mat prev_grey_, grey_;
-               //  resize(prev_grey, prev_grey_, Size(453, 342));
-               //  resize(grey, grey_, Size(453, 342));
 		frame_0.upload(prev_grey);
 		frame_1.upload(grey);
 
-
-        // GPU optical flow
-		switch(type){
-		case 0:
-			alg_farn(frame_0,frame_1,flow_u,flow_v);
-			break;
-		case 1:
-			alg_tvl1(frame_0,frame_1,flow_u,flow_v);
-			break;
-		case 2:
-			GpuMat d_frame0f, d_frame1f;
-	        frame_0.convertTo(d_frame0f, CV_32F, 1.0 / 255.0);
-	        frame_1.convertTo(d_frame1f, CV_32F, 1.0 / 255.0);
-			alg_brox(d_frame0f, d_frame1f, flow_u,flow_v);
-			break;
-		}
+		alg_tvl1(frame_0, frame_1, flow_u, flow_v);
 
 		flow_u.download(flow_x);
 		flow_v.download(flow_y);
-
-		// Output optical flow
-		Mat imgX(flow_x.size(),CV_8UC1);
-		Mat imgY(flow_y.size(),CV_8UC1);
-		convertFlowToImage(flow_x,flow_y, imgX, imgY, -bound, bound);
+		
+		// output optical flow
+		if(k < proposed_imageNum){
+		k = k+1;
+		Mat imgX(flow_x.size(), CV_8UC1);
+		Mat imgY(flow_y.size(), CV_8UC1);
+		convertFlowToImage(flow_x, flow_y, imgX, imgY, -bound, bound);
 		char tmp[20];
-		sprintf(tmp,"_%05d.jpg",int(frame_num));
+		sprintf(tmp, "_%05d.jpg", int(frame_num));
 
-		// Mat imgX_, imgY_, image_;
-		// resize(imgX,imgX_,cv::Size(340,256));
-		// resize(imgY,imgY_,cv::Size(340,256));
-		// resize(image,image_,cv::Size(340,256));
-
-		imwrite(xFlowFile + tmp,imgX);
-		imwrite(yFlowFile + tmp,imgY);
-		imwrite(imgFile + tmp, image);
-
+		imwrite(dirName + '/' +'X' + tmp, imgX);
+		imwrite(dirName + '/' +'Y' + tmp, imgY);
+		}
+		else
+			break;
+		//cout<<dirName<<tmp<<endl;
 		std::swap(prev_grey, grey);
 		std::swap(prev_image, image);
 		frame_num = frame_num + 1;
 
 		int step_t = step;
-		while (step_t > 1){
+		while (step_t > 1) {
 			capture >> frame;
 			step_t--;
 		}
+	}
+}
+
+
+int main(int argc, char** argv){
+	// IO operation
+const char* keys =
+		{
+			"{ f  | vidFile      | ex2.avi | filename of video }"
+			"{ start  | start    | flow_x | filename of flow x component }"
+			"{ end  | end    | flow_y | filename of flow x component }"
+		//	"{ i  | imgFile      | flow_i | filename of flow image}"
+			"{ b  | bound | 15 | specify the maximum of optical flow}"
+		//	"{ t  | type | 0 | specify the optical flow algorithm }"
+			"{ d  | device_id    | 0  | set gpu id}"
+			"{ s  | step  | 1 | specify the step for frame sampling}"
+		};
+
+	CommandLineParser cmd(argc, argv, keys);
+	string vidFile = cmd.get<string>("vidFile");
+	int start = cmd.get<int>("start");
+	int end = cmd.get<int>("end");
+	//string imgFile = cmd.get<string>("imgFile");
+	int bound = cmd.get<int>("bound");
+    int type  = cmd.get<int>("type");
+    int device_id = cmd.get<int>("device_id");
+    int step = cmd.get<int>("step");
+
+	vector<string> file_names;
+	sort(file_names.begin(), file_names.end());
+	string path = vidFile;
+
+	GetFileNames(path, file_names);
+	for(int i = start; i<end; i++)
+	{
+		cout<<"start: "<<file_names[i]<<endl;
+		PorcessOneVideo(file_names[i], device_id, step, bound);
+		
 	}
 	return 0;
 }
